453fcd1e16cc30bd791003014134230f23552adf
RAKwireless <frank.lu@rakwireless.com>
2017-11-17 10:48:31 +0800

    Modify the definition of the serial port

--- Arduino/RAK811/examples/JoinNetworkOTAA/JoinNetworkOTAA.ino
+++ Arduino/RAK811/examples/JoinNetworkOTAA/JoinNetworkOTAA.ino
@@ -13,53 +13,54 @@ String NwkSKey = "3432567afde4525e7890cfea234a5821";
 String AppSKey = "a48adfc393a0de458319236537a11d90";
 String DevAddr = "00112233";
 #endif
-#define TXpin 1   // Set the virtual serial port pins
-#define RXpin 0
-SoftwareSerial mySerial(RXpin,TXpin);    // Declare a virtual serial port
+#define TXpin 11   // Set the virtual serial port pins
+#define RXpin 10
+#define ATSerial Serial 
+SoftwareSerial DebugSerial(RXpin,TXpin);    // Declare a virtual serial port
 char* buffer = "72616B776972656C657373";
 
-RAK811 RAKLoRa(mySerial);
+RAK811 RAKLoRa(ATSerial);
 
 
 
 void setup() {
- Serial.begin(38400);
- while(Serial.read()>= 0) {}
- while(!Serial);
- Serial.println("StartUP");
+ DebugSerial.begin(115200);
+ while(DebugSerial.read()>= 0) {}
+ while(!DebugSerial);
+ DebugSerial.println("StartUP");
 
- mySerial.begin(9600);
+ ATSerial.begin(115200);
 
 }
 
 void loop() {
  
- Serial.println(RAKLoRa.rk_getVersion());
+ DebugSerial.println(RAKLoRa.rk_getVersion());
  delay(500);
- Serial.println(RAKLoRa.rk_getBand());
+ DebugSerial.println(RAKLoRa.rk_getBand());
  delay(500);
  if(RAKLoRa.rk_setWorkingMode(WORK_MODE))
  {
-  Serial.println("you set Working mode is OK!");
+  DebugSerial.println("you set Working mode is OK!");
   if (RAKLoRa.rk_initOTAA(DevEui,AppEui,AppKey))
     {
-      Serial.println("you init OTAA parameter is OK!");
+      DebugSerial.println("you init OTAA parameter is OK!");
       if (RAKLoRa.rk_joinLoRaNetwork(JOIN_MODE))
        {
-           Serial.println("you join Network is OK!");
-           mySerial.setTimeout(8000);
-           String ver = mySerial.readStringUntil('\n');
-           Serial.println(ver);
-           mySerial.setTimeout(2000);
+           DebugSerial.println("you join Network is OK!");
+           ATSerial.setTimeout(8000);
+           String ver = ATSerial.readStringUntil('\n');
+           DebugSerial.println(ver);
+           ATSerial.setTimeout(2000);
            if (ver == STATUS_JOINED_SUCCESS)
            {
             while (RAKLoRa.rk_sendData(1,1,buffer))
             {
-              Serial.println(RAKLoRa.rk_recvData());
+              DebugSerial.println(RAKLoRa.rk_recvData());
             }
            }
         }
      }
  }
- delay(500);
+ delay(5000);
 }
